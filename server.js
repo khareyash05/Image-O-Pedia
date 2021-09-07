0eed2f0617537b184f68113a028c3fdebcdb2926
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-09-07 17:10:08 +0530

    file management

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"))
 // a get route to get all images from the database
 app.get('/', async (req, res) => {
     res.redirect('/'+1)
